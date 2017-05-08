125c5bf4a06df6a8623c6e24624435923e354a48
Paul Billings <paulbillings101@yahoo.co.uk>
2017-05-08 16:32:07 +0100

    question2c, linked the html and js and tested - worked

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,54 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+/* global plugin */
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+/**
+ * Utility to get default value from the field name if it was undefined or empty
+ * @param {type} fieldName
+ * @param {type} defaultValue
+ * @returns {jQuery}
+ */
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
+function isNumber(str) {
+    return str.length === 1 && str.match(/[0-9]/);
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+function get_name_value(fieldName, defaultValue) {
+    var value = $('#' + fieldName).val();
+    if (value == "") {
+       value = defaultValue;
+       $('#' + fieldName).val(value);
     }
-};
+    if (fieldName == "name") {
+        if (!(isLetter(value.charAt(0)) && isNumber(value.charAt(value.length - 1)))) {
+            alert("Please enter the correct value");
+            return "";
+        }
+    }
+    return value;
+}
 
+/**
+ * This is the main class
+ */
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+      var address;
+      function megaMaxSale() {
+		// code to go here
+       
+	    this.previousWidget = function () {
+	    alert("In previous widget");
+	  };
+	   
+
+	   
+      } //end of megaMaxSale function
+      this.megaMaxSale = new megaMaxSale();
+    }    
+};
 app.initialize();
