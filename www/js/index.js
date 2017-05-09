426b12b5648decd4bf13c87d7a01ac1d534f68ff
Paul Billings <paulbillings101@yahoo.co.uk>
2017-05-09 11:24:29 +0100

    done FR1.1 validate oucu

--- www/js/index.js
+++ www/js/index.js
@@ -2,27 +2,25 @@
 
 
 /**
- * Utility to get default value from the field name if it was undefined or empty
+ * Utility to get value from the field name and check it is valid
  * @param {type} fieldName
- * @param {type} defaultValue
  * @returns {jQuery}
  */
-function isLetter(str) {
+ 
+//Funtional requirement FR1.1
+ function isLetter(str) {
     return str.length === 1 && str.match(/[a-z]/i);
 }
 function isNumber(str) {
     return str.length === 1 && str.match(/[0-9]/);
 }
 
-function get_name_value(fieldName, defaultValue) {
+function get_name_value(fieldName) {
     var value = $('#' + fieldName).val();
-    if (value == "") {
-       value = defaultValue;
-       $('#' + fieldName).val(value);
-    }
-    if (fieldName == "name") {
+    
+    if (fieldName == "salesperson") {
         if (!(isLetter(value.charAt(0)) && isNumber(value.charAt(value.length - 1)))) {
-            alert("Please enter the correct value");
+            alert("Please enter a valid Salesperson ID in the correct format");
             return "";
         }
     }
@@ -42,27 +40,32 @@ var app = {
 		// code to go here
        
 	    this.previousWidget = function () {
+			
+			//get salesperson and password
+			var oucu = get_name_value('salesperson'); 
+			var pass = $('#' + 'password').val();
+		
 			widgetNumber--;
 			if (widgetNumber < 0) {
 				alert('Reached beginning of list');
 				widgetNumber = 0;
 			}
-	    
+			
             /* Invoke the RESTful API to get widget details*/
-			$.get('http://137.108.93.222/openstack/api/widgets?OUCU=pb8255&password=CKJ8SgSY',
+			$.get('http://137.108.93.222/openstack/api/widgets?OUCU='+ oucu + '&password=' + pass,
               function (data) {
                   var obj = $.parseJSON(data);
                   if (obj.status == "error") {
                       alert(obj.data[0].reason);
                   } else {
-						//alert(obj.data[widgetNumber].description);
+						//display widget image
 						var image = document.getElementById("widget_image");
 						image.style.display = "block";
 						image.src = obj.data[widgetNumber].url;
-						
+						//display description
 						var disc = obj.data[widgetNumber].description;
 						document.getElementById('description').innerHTML = disc;
-						
+						//display price
 						var price = obj.data[widgetNumber].pence_price;
 						document.getElementById('price').innerHTML = price;
 					}
@@ -76,24 +79,27 @@ var app = {
 				alert('Reached end of list');
 				widgetNumber = 9;
 			}
+			//get salesperson and password
+			var oucu = get_name_value('salesperson'); 
+			var pass = $('#' + 'password').val();
 	    
             /* Invoke the RESTful API to get widget details*/
-			$.get('http://137.108.93.222/openstack/api/widgets?OUCU=pb8255&password=CKJ8SgSY',
+			$.get('http://137.108.93.222/openstack/api/widgets?OUCU='+ oucu + '&password=' + pass,
               function (data) {
                   var obj = $.parseJSON(data);
                   if (obj.status == "error") {
                       alert(obj.data[0].reason);
                   } else {
-						//alert(obj.data[widgetNumber].description);
+						//display widget image
 						var image = document.getElementById("widget_image");
 						image.style.display = "block";
 						image.src = obj.data[widgetNumber].url;
-						
+						//display description
 						var disc = obj.data[widgetNumber].description;
 						document.getElementById('description').innerHTML = disc;
-						
+						//display price
 						var price = obj.data[widgetNumber].pence_price;
-						document.getElementById('price').value = "Price = " + price + " p";
+						document.getElementById('price').value = "Price = " + price + "p";
 					}
 			  });
 	  };
